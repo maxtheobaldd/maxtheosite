7fa0b22677f23e7f3b67a93b47a515374ee21956
Max Theobald <56725995+maxtheobaldd@users.noreply.github.com>
2022-11-03 14:54:44 +0000

    Update Footer.js
    Updated the footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        Max Theobald's Website, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">maxtheo.tk</Link>
       </p>
     </div>
   </footer>
