604c7ae571eaa40f00de70cbe144787d0ab03136
Max Theobald <maxtheobald@protonmail.com>
2022-11-04 08:21:53 +0000

    Update footer.js
    Updating the footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ const Footer = () => (
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        -<Link to="/">maxtheo.tk</Link>
+        - <Link to="/">maxtheo.tk</Link>
       </p>
     </div>
   </footer>
